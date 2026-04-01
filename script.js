34bae221667a93615e88b9d75d8b8015aff79c43
Sreekar Reddy <112748777+sreekar-2002@users.noreply.github.com>
2026-04-01 18:10:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function happyMood() {
+  document.getElementById("body").style.backgroundColor = "yellow";
+  document.getElementById("message").innerText = "Yay! Keep smiling \u{1F60A}";
+}
+
+
+
+
+function sadMood() {
+  document.getElementById("body").style.backgroundColor = "blue";
+  document.getElementById("message").innerText ="It's okay to feel sad sometimes.., come back soon \u{1F622}";
+}
+
+
+
+
+function angryMood() {
+  document.getElementById("body").style.backgroundColor = "red";
+  document.getElementById("message").innerText = "Calm down, don't be angry \u{1F621}";
+}
+
+
+   
+
+function relaxedMood() {
+  document.getElementById("body").style.backgroundColor = "green";
+  document.getElementById("message").innerText = "Stay calm and relaxed \u{1F60C}";
+}
